ea8d2fda2db2dfe81a496f4e234c954f6a0d69b5
beuhmomeni <139468523+beuhmomeni@users.noreply.github.com>
2023-08-09 20:24:10 +0700

    Create botv1.py
    kodland 986 main.py's code

---
+++ botv1.py
@@ -0,0 +1,72 @@
+import discord
+import random
+import random, asyncio
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
+    else:
+        return "TAILS"
+    
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah masuk sebagai {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+
+    elif message.content.startswith('$guess'):
+        await message.channel.send('Guess a number between 1 and 10.')
+
+        def is_correct(m):
+            return m.author == message.author and m.content.isdigit()
+
+        answer = random.randint(1, 10)
+
+        try:
+            guess = await client.wait_for('message', check=is_correct, timeout=5.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+
+        if int(guess.content) == answer:
+            await message.channel.send('You are right!')
+        else:
+            await message.channel.send(f'Oops. It is actually {answer}.')
+
+
+
+client.run("")
