ed9f9b902ab4a1ac0b1f6d7e08f2e4d0d4c500de
beuhmomeni <139468523+beuhmomeni@users.noreply.github.com>
2023-08-14 19:38:52 +0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -45,6 +45,30 @@ async def animals(ctx):
    # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
     await ctx.send(file=picture)
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 
 bot.run("")
